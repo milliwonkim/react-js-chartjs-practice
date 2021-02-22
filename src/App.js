e57f3e905fb1513236043d6c398fd6a262e35121
milliwonkim <rldnjs9347@gmail.com>
2021-02-22 20:53:38 +0900

    Revert "[add] add radar chart"
    This reverts commit cf4ae280d33e62aeb4b3a5b79476219b97e7e9ba.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useRef, useEffect, useState } from 'react'
-import Radar from './Radar'
 import StackedBarChart from './StackedBarChart'
 
 function App() {
     return (
         <div className="App">
             <StackedBarChart />
-            <Radar />
         </div>
     )
 }
